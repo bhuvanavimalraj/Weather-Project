e53fc82d120f9d75c7a621dbebe7e1b7f69f63c4
Bhuvaneswari Vimalraj <sgbhuvanait@gmail.com>
2021-05-01 18:54:24 +0800

    created css, Js & Jpg file

---
+++ style.js
@@ -0,0 +1,119 @@
+let weather = {
+  paris: {
+    temp: 19.7,
+    humidity: 80,
+  },
+  tokyo: {
+    temp: 17.3,
+    humidity: 50,
+  },
+  lisbon: {
+    temp: 30.2,
+    humidity: 20,
+  },
+  "san francisco": {
+    temp: 20.9,
+    humidity: 100,
+  },
+  moscow: {
+    temp: -5,
+    humidity: 20,
+  },
+};
+
+//Code is here
+// let city = prompt("Enter a city?");
+
+// if (weather[city] !== undefined) {
+//   let temp = weather[city].temp;
+//   let humidity = weather[city].humidity;
+//   let celsiustemp = Math.round(temp);
+//   let fahrenheit = Math.round((temp * 9) / 5 + 32);
+
+//   alert(
+//     `It is currently ${celsiustemp}°C (${fahrenheit}°F) in ${city} with a humidity of ${humidity}%`
+//   );
+// } else {
+//   alert(
+//     `Sorry we do not know the weather for ${city}, try going to https://www.google.com/search?q=weather+${city}.`
+//   );
+// }
+// Searching function here
+let apiKey = "a3a52cc6f810ba9bb9deaaefe37a7628";
+
+function update(response) {
+  let { data } = response;
+  //console.log(data.name, data.main.temp);
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${data.name}`;
+  let temperature = document.querySelector("#temperature-value");
+  temperature.innerHTML = Math.round(data.main.temp);
+}
+
+function search(event) {
+  event.preventDefault();
+
+  let searchInput = document.querySelector("#search-input");
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&units=metric`;
+
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(update);
+
+  // if (searchInput.value && weather[searchInput.value.toLowerCase()]) {
+  //   // h1.innerHTML = `${searchInput.value}`;
+  //   // let temperature = document.querySelector("#temperature-value");
+  //   // temperature.innerHTML = weather[searchInput.value.toLowerCase()].temp;
+  // } else {
+  //   h1.innerHTML = null;
+  // }
+}
+
+// let temperatureElement = document.querySelector("h1");
+//   temperatureElement.innerHTML = `It is ${temperature} degrees in Sydney`;
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
+
+// Date update here
+let now = new Date();
+let h6 = document.querySelector("h6");
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+// console.log(days[3]);
+let day = days[now.getDay()];
+
+let hours = now.getHours();
+
+let minutes = now.getMinutes();
+
+h6.innerHTML = `${day} ${hours}:${minutes}<br>Partly Cloudy</br>`;
+console.log(hours);
+
+// Celsius and Fahrenheit Calculate
+function calculateCelsius(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let temperature = document.querySelector("#temperature-value");
+  temperature.innerHTML = weather[searchInput.value.toLowerCase()].temp;
+}
+
+function calculateFahrenheit(event) {
+  event.preventDefault();
+  let temperature = document.querySelector("#temperature-value");
+  let fahrenheit = Math.round((+temperature.innerText * 9) / 5 + 32);
+  temperature.innerHTML = fahrenheit;
+}
+
+let celsius = document.querySelector("#celsius-link");
+celsius.addEventListener("click", calculateCelsius);
+
+let fahrenheit = document.querySelector("#fahrenheit-link");
+fahrenheit.addEventListener("click", calculateFahrenheit);
+
+//Geolocation API here
