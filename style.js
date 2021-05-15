5f283463e155883b51f94ffa536a33c077ebdc33
Bhuvaneswari Vimalraj <sgbhuvanait@gmail.com>
2021-05-15 19:40:21 +0800

    Fixed all the issues and made some style changes

--- style.js
+++ style.js
@@ -1,10 +1,23 @@
 // Update function here
 let apiKey = "a3a52cc6f810ba9bb9deaaefe37a7628";
 
+// DOM Elements
+let temperatureElement = document.querySelector("#temp");
+let celsius = document.querySelector("#celsius-link");
+let form = document.querySelector("#search-form");
+let fahrenheit = document.querySelector("#fahrenheit-link");
+
+// Event Listeners
+form.addEventListener("submit", search);
+celsius.addEventListener("click", calculateCelsius);
+fahrenheit.addEventListener("click", calculateFahrenheit);
+
+// Methods
+
 function getForecast(coordinates) {
   console.log(coordinates);
 
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unit=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   console.log(apiUrl);
 
   axios.get(apiUrl).then(displayForecast);
@@ -25,8 +38,9 @@ function update(response) {
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = `${data.name}`;
 
-  let temperature = document.querySelector("#temperature-value > span");
-  temperature.innerHTML = Math.round(response.data.main.temp);
+  celsiusTemperature = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
 
   let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = `${data.weather[0].description}`;
@@ -78,6 +92,7 @@ function formatDate(timestamp) {
 
   return `${day} ${hours}:${minutes}`;
 }
+
 function search(event) {
   event.preventDefault();
 
@@ -88,28 +103,28 @@ function search(event) {
   axios.get(`${apiUrl}&appid=${apiKey}`).then(update);
 }
 
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", search);
-
 // Celsius and Fahrenheit Calculate
 function calculateCelsius(event) {
   event.preventDefault();
-  let temperature = document.querySelector("#temperature-value");
-  temperature.innerHTML = globalTemperature;
+
+  celsius.classList.add("active");
+  fahrenheit.classList.remove("active");
+
+  // let temperature = document.querySelector("#temperature-value");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
 function calculateFahrenheit(event) {
   event.preventDefault();
-  let temperature = document.querySelector("#temperature-value");
-  let fahrenheit = Math.round((+temperature.innerText * 9) / 5 + 32);
-  temperature.innerHTML = fahrenheit;
+
+  // let temperature = document.querySelector("#temperature-value");
+
+  celsius.classList.remove("active");
+  fahrenheit.classList.add("active");
+
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
-
-let celsius = document.querySelector("#celsius-link");
-celsius.addEventListener("click", calculateCelsius);
-
-let fahrenheit = document.querySelector("#fahrenheit-link");
-fahrenheit.addEventListener("click", calculateFahrenheit);
 
 // weather forecast function start here
 function formatDay(timestamp) {
@@ -120,6 +135,7 @@ function formatDay(timestamp) {
 }
 
 function displayForecast(response) {
+  console.log(response);
   let forecast = response.data.daily;
 
   let forecastElement = document.querySelector("#forecast");
@@ -139,13 +155,14 @@ function displayForecast(response) {
         <img src="http://openweathermap.org/img/wn/${
           forecastDay.weather[0].icon
         }@2x.png" alt="" width="42"/>
+        <br />
 
         <span class="weather-forecast-temperatures-max">${Math.round(
           forecastDay.temp.max
-        )}℃</span>
+        )} &deg;<span style="font-size:12px">C</span></span>
         <span class="weather-forecast-temperatures-min">${Math.round(
           forecastDay.temp.min
-        )}℃</span>
+        )} &deg;<span style="font-size:12px">C</span></span>
       </div>
       `;
     }
